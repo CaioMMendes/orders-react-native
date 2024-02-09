04a41f91cc03906d5244abfdc6a9ca49fabf3f42
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-02-09 16:11:46 -0300

    :sparkles: feat: Add category button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,34 @@
+import CategoryButton from "@/components/category-button";
 import Header from "@/components/header";
-import { View, Text } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
+import { View, Text, FlatList } from "react-native";
 
 export default function Home() {
+  const [categorySelected, setCategorySelected] = useState(CATEGORIES[0]);
+
+  const handleCategorySelect = (category: string) => {
+    setCategorySelected(category);
+  };
+
   return (
     <View className="flex-1  pt-8">
       <Header title={"Faça seu pedido"} />
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={(item) => (
+          <CategoryButton
+            title={item.item}
+            isSelected={item.item === categorySelected}
+            onPress={() => handleCategorySelect(item.item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
